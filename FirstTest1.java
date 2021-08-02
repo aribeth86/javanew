742b38960b1949ffadb16b8345e197bd6a881bf3
aribeth86 <82219480+aribeth86@users.noreply.github.com>
2021-08-02 18:07:50 +0700

    Add files via upload

--- FirstTest1.java
+++ FirstTest1.java
@@ -1,17 +1,24 @@
-public class FirstTest1{
-	
-	public static void main (String[] args) {
-		System.out.println ("We are here! Hello! ");
-		System.out.println (8 + 2);
-		System.out.println (8 - 2);
-		System.out.println (8 * 2);
-		System.out.println (8 / 3);
+public class FirstTest1 {
 
-		System.out.println (8.0 / 3);
+    public static void main(String[] args) {
+        helloTest();
+        helloTest("username");
+        System.out.println(helloTest("значение1", "Значение2"));
+    }
 
+    /*демо методов*/
+    //1 метод без параметров
+    public static void helloTest() {
+        System.out.println("We ara here, hello!");
+    }
 
-		double side =7.45;
-		String text="Площадь квадрата со стороной ";
-		System.out.println(text+side+ "="+(side*side));
-	}
+    //2 метод с параметром
+    public static void helloTest(String value) {
+        System.out.println("We ara here, hello!" + value);
+    }
+
+    //3 метод с возвратом значения
+    public static String helloTest(String value1, String value2) {
+        return "We ara here, hello!" + value1 + ", " + value2;
+    }
 }
