446db2842e3bd90d9e9fbd73103a8790f5930475
aribeth86 <82219480+aribeth86@users.noreply.github.com>
2021-08-02 16:12:56 +0700

    Add files via upload

---
+++ FirstTest1.java
@@ -0,0 +1,17 @@
+public class FirstTest1{
+	
+	public static void main (String[] args) {
+		System.out.println ("We are here! Hello! ");
+		System.out.println (8 + 2);
+		System.out.println (8 - 2);
+		System.out.println (8 * 2);
+		System.out.println (8 / 3);
+
+		System.out.println (8.0 / 3);
+
+
+		double side =7.45;
+		String text="Площадь квадрата со стороной ";
+		System.out.println(text+side+ "="+(side*side));
+	}
+}
