43f9dbe3386f87754b9880f3c819e8ac03d6abe5
aribeth86 <aribeth@list.ru>
2021-08-03 22:44:44 +0700

    branch2. Java comander

--- src/main/java/FirstTest1.java
+++ src/main/java/FirstTest1.java
@@ -1,24 +1,16 @@
+import test.ship.StarShip;
+
 public class FirstTest1 {
 
     public static void main(String[] args) {
-        helloTest();
-        helloTest("username");
-        System.out.println(helloTest("значение1", "Значение2"));
+        //создаю экземпляр класса корабля
+        StarShip ship = new StarShip();
+
+        //задаю конкретному кораблю конкретное свойство
+        ship.shipHeight = 253.25;
+    }
+    public static void displayHeight(StarShip ourship) {
+        System.out.println("Высота корабля составляет : "+ourship.shipHeight);
     }
 
-    /*демо методов*/
-    //1 метод без параметров
-    public static void helloTest() {
-        System.out.println("We ara here, hello!");
-    }
-
-    //2 метод с параметром
-    public static void helloTest(String value) {
-        System.out.println("We ara here, hello!" + value);
-    }
-
-    //3 метод с возвратом значения
-    public static String helloTest(String value1, String value2) {
-        return "We ara here, hello!" + value1 + ", " + value2;
-    }
 }
---
+++ src/main/java/test/ship/StarShip.java
@@ -0,0 +1,8 @@
+package test.ship;
+/*Created by Elena 08 2021*/
+
+public class StarShip {
+    /*высота*/
+    public double shipHeight;
+
+}
